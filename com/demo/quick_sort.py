d56ee14cb60e6d900c3e8ee819f191c2a0963ad2
Wangsx <wangsixxx@sina.com>
2018-01-22 17:19:48 +0800

    learn basic algorithm

---
+++ com/demo/quick_sort.py
@@ -0,0 +1,16 @@
+def quick_sort(array):
+    # 基线条件，如果数组为空或只有一个元素，即有序元素直接取值
+    if len(array) < 2:
+        return array
+    # 递归条件
+    else:
+        # 使用第一个元素作为基本值
+        pivot = array[0]
+        # 由所有小于基本值的元素组成的数组
+        less = [i for i in array[1:] if i <= pivot]
+        # 由所有大于基本值的元素组成的数组
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print(quick_sort([10, 4, 3, 2, 5]))
